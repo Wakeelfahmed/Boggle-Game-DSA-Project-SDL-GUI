0848ce0b77b2e8b6c6d2bdde212c6e93d962bbd5
Wakeel Furqan Ahmed <wakeelfahmed@gmail.com>
2022-12-30 21:24:32 +0500

    SDL2_gfxPrimitives functional.

--- Boggle Solver SDL/Main.cpp
+++ Boggle Solver SDL/Main.cpp
@@ -3,231 +3,230 @@
 #include<conio.h>
 #include"include/SDL.h"
 #include"include/SDL_ttf.h"
+#include"include/SDL2_gfxPrimitives.h"
 //#include"include/SDL_render.h"
 //#include"include/SDL_shape.h"
-//#include"include/SDL2_gfxPrimitives.h"
 //#include"include/"
 //#include"include/SDL_"
-//#include<include/SDL2_gfxPrimitives.h >
-#include"include/SDL_assert.h"
+//#include<include/SDL2_gfxPrimitives.c >
+//#include"include/SDL_assert.h"
 #define DEFAULT_ELLIPSE_OVERSCAN 4
 int Transparency = 90;
-int pixel(SDL_Renderer* renderer, Sint16 x, Sint16 y)
-{
-	return SDL_RenderDrawPoint(renderer, x, y);
-}
-int vline(SDL_Renderer* renderer, Sint16 x, Sint16 y1, Sint16 y2)
-{
-	return SDL_RenderDrawLine(renderer, x, y1, x, y2);;
-}
-int hline(SDL_Renderer* renderer, Sint16 x1, Sint16 x2, Sint16 y)
-{
-	return SDL_RenderDrawLine(renderer, x1, y, x2, y);;
-}
-int _drawQuadrants(SDL_Renderer* renderer, Sint16 x, Sint16 y, Sint16 dx, Sint16 dy, Sint32 f)
-{
-	int result = 0;
-	Sint16 xpdx, xmdx;
-	Sint16 ypdy, ymdy;
-
-	if (dx == 0) {
-		if (dy == 0) {
-			result |= pixel(renderer, x, y);
-		}
-		else {
-			ypdy = y + dy;
-			ymdy = y - dy;
-			if (f) {
-				result |= vline(renderer, x, ymdy, ypdy);
-			}
-			else {
-				result |= pixel(renderer, x, ypdy);
-				result |= pixel(renderer, x, ymdy);
-			}
-		}
-	}
-	else {
-		xpdx = x + dx;
-		xmdx = x - dx;
-		ypdy = y + dy;
-		ymdy = y - dy;
-		if (f) {
-			result |= vline(renderer, xpdx, ymdy, ypdy);
-			result |= vline(renderer, xmdx, ymdy, ypdy);
-		}
-		else {
-			result |= pixel(renderer, xpdx, ypdy);
-			result |= pixel(renderer, xmdx, ypdy);
-			result |= pixel(renderer, xpdx, ymdy);
-			result |= pixel(renderer, xmdx, ymdy);
-		}
-	}
-
-	return result;
-}
-int _ellipseRGBA(SDL_Renderer* renderer, Sint16 x, Sint16 y, Sint16 rx, Sint16 ry, Uint8 r, Uint8 g, Uint8 b, Uint8 a, Sint32 f)
-{
-	int result;
-	Sint32 rxi, ryi;
-	Sint32 rx2, ry2, rx22, ry22;
-	Sint32 error;
-	Sint32 curX, curY, curXp1, curYm1;
-	Sint32 scrX, scrY, oldX, oldY;
-	Sint32 deltaX, deltaY;
-	Sint32 ellipseOverscan;
-
-	/*
-	* Sanity check radii
-	*/
-	if ((rx < 0) || (ry < 0)) {
-		return (-1);
-	}
-
-	/*
-	* Set color
-	*/
-	result = 0;
-	result |= SDL_SetRenderDrawBlendMode(renderer, (a == 255) ? SDL_BLENDMODE_NONE : SDL_BLENDMODE_BLEND);
-	result |= SDL_SetRenderDrawColor(renderer, r, g, b, a);
-
-	/*
-	* Special cases for rx=0 and/or ry=0: draw a hline/vline/pixel
-	*/
-	if (rx == 0) {
-		if (ry == 0) {
-			return (pixel(renderer, x, y));
-		}
-		else {
-			return (vline(renderer, x, y - ry, y + ry));
-		}
-	}
-	else {
-		if (ry == 0) {
-			return (hline(renderer, x - rx, x + rx, y));
-		}
-	}
-
-	/*
-	 * Adjust overscan
-	 */
-	rxi = rx;
-	ryi = ry;
-	if (rxi >= 512 || ryi >= 512)
-	{
-		ellipseOverscan = DEFAULT_ELLIPSE_OVERSCAN / 4;
-	}
-	else if (rxi >= 256 || ryi >= 256)
-	{
-		ellipseOverscan = DEFAULT_ELLIPSE_OVERSCAN / 2;
-	}
-	else
-	{
-		ellipseOverscan = DEFAULT_ELLIPSE_OVERSCAN / 1;
-	}
-
-	/*
-	 * Top/bottom center points.
-	 */
-	oldX = scrX = 0;
-	oldY = scrY = ryi;
-	result |= _drawQuadrants(renderer, x, y, 0, ry, f);
-
-	/* Midpoint ellipse algorithm with overdraw */
-	rxi *= ellipseOverscan;
-	ryi *= ellipseOverscan;
-	rx2 = rxi * rxi;
-	rx22 = rx2 + rx2;
-	ry2 = ryi * ryi;
-	ry22 = ry2 + ry2;
-	curX = 0;
-	curY = ryi;
-	deltaX = 0;
-	deltaY = rx22 * curY;
-
-	/* Points in segment 1 */
-	error = ry2 - rx2 * ryi + rx2 / 4;
-	while (deltaX <= deltaY)
-	{
-		curX++;
-		deltaX += ry22;
-
-		error += deltaX + ry2;
-		if (error >= 0)
-		{
-			curY--;
-			deltaY -= rx22;
-			error -= deltaY;
-		}
-
-		scrX = curX / ellipseOverscan;
-		scrY = curY / ellipseOverscan;
-		if ((scrX != oldX && scrY == oldY) || (scrX != oldX && scrY != oldY)) {
-			result |= _drawQuadrants(renderer, x, y, scrX, scrY, f);
-			oldX = scrX;
-			oldY = scrY;
-		}
-	}
-
-	/* Points in segment 2 */
-	if (curY > 0)
-	{
-		curXp1 = curX + 1;
-		curYm1 = curY - 1;
-		error = ry2 * curX * curXp1 + ((ry2 + 3) / 4) + rx2 * curYm1 * curYm1 - rx2 * ry2;
-		while (curY > 0)
-		{
-			curY--;
-			deltaY -= rx22;
-
-			error += rx2;
-			error -= deltaY;
-
-			if (error <= 0)
-			{
-				curX++;
-				deltaX += ry22;
-				error += deltaX;
-			}
-
-			scrX = curX / ellipseOverscan;
-			scrY = curY / ellipseOverscan;
-			if ((scrX != oldX && scrY == oldY) || (scrX != oldX && scrY != oldY)) {
-				oldY--;
-				for (; oldY >= scrY; oldY--) {
-					result |= _drawQuadrants(renderer, x, y, scrX, oldY, f);
-					/* prevent overdraw */
-					if (f) {
-						oldY = scrY - 1;
-					}
-				}
-				oldX = scrX;
-				oldY = scrY;
-			}
-		}
-
-		/* Remaining points in vertical */
-		if (!f) {
-			oldY--;
-			for (; oldY >= 0; oldY--) {
-				result |= _drawQuadrants(renderer, x, y, scrX, oldY, f);
-			}
-		}
-	}
-
-	return (result);
-}
-int filledEllipseRGBA(SDL_Renderer* renderer, Sint16 x, Sint16 y, Sint16 rx, Sint16 ry, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
-{
-	return _ellipseRGBA(renderer, x, y, rx, ry, r, g, b, a, 1);
-}
-int filledCircleColor(SDL_Renderer* renderer, Sint16 x, Sint16 y, Sint16 rad, Uint32 color)
-{
-	Uint8* c = (Uint8*)&color;
-	return filledEllipseRGBA(renderer, x, y, rad, rad, c[0], c[1], c[2], c[3]);
-}
+//int pixel(SDL_Renderer* renderer, Sint16 x, Sint16 y)
+//{
+//	return SDL_RenderDrawPoint(renderer, x, y);
+//}
+//int vline(SDL_Renderer* renderer, Sint16 x, Sint16 y1, Sint16 y2)
+//{
+//	return SDL_RenderDrawLine(renderer, x, y1, x, y2);;
+//}
+//int hline(SDL_Renderer* renderer, Sint16 x1, Sint16 x2, Sint16 y)
+//{
+//	return SDL_RenderDrawLine(renderer, x1, y, x2, y);;
+//}
+//int _drawQuadrants(SDL_Renderer* renderer, Sint16 x, Sint16 y, Sint16 dx, Sint16 dy, Sint32 f)
+//{
+//	int result = 0;
+//	Sint16 xpdx, xmdx;
+//	Sint16 ypdy, ymdy;
+//
+//	if (dx == 0) {
+//		if (dy == 0) {
+//			result |= pixel(renderer, x, y);
+//		}
+//		else {
+//			ypdy = y + dy;
+//			ymdy = y - dy;
+//			if (f) {
+//				result |= vline(renderer, x, ymdy, ypdy);
+//			}
+//			else {
+//				result |= pixel(renderer, x, ypdy);
+//				result |= pixel(renderer, x, ymdy);
+//			}
+//		}
+//	}
+//	else {
+//		xpdx = x + dx;
+//		xmdx = x - dx;
+//		ypdy = y + dy;
+//		ymdy = y - dy;
+//		if (f) {
+//			result |= vline(renderer, xpdx, ymdy, ypdy);
+//			result |= vline(renderer, xmdx, ymdy, ypdy);
+//		}
+//		else {
+//			result |= pixel(renderer, xpdx, ypdy);
+//			result |= pixel(renderer, xmdx, ypdy);
+//			result |= pixel(renderer, xpdx, ymdy);
+//			result |= pixel(renderer, xmdx, ymdy);
+//		}
+//	}
+//
+//	return result;
+//}
+//int _ellipseRGBA(SDL_Renderer* renderer, Sint16 x, Sint16 y, Sint16 rx, Sint16 ry, Uint8 r, Uint8 g, Uint8 b, Uint8 a, Sint32 f)
+//{
+//	int result;
+//	Sint32 rxi, ryi;
+//	Sint32 rx2, ry2, rx22, ry22;
+//	Sint32 error;
+//	Sint32 curX, curY, curXp1, curYm1;
+//	Sint32 scrX, scrY, oldX, oldY;
+//	Sint32 deltaX, deltaY;
+//	Sint32 ellipseOverscan;
+//
+//	/*
+//	* Sanity check radii
+//	*/
+//	if ((rx < 0) || (ry < 0)) {
+//		return (-1);
+//	}
+//
+//	/*
+//	* Set color
+//	*/
+//	result = 0;
+//	result |= SDL_SetRenderDrawBlendMode(renderer, (a == 255) ? SDL_BLENDMODE_NONE : SDL_BLENDMODE_BLEND);
+//	result |= SDL_SetRenderDrawColor(renderer, r, g, b, a);
+//
+//	/*
+//	* Special cases for rx=0 and/or ry=0: draw a hline/vline/pixel
+//	*/
+//	if (rx == 0) {
+//		if (ry == 0) {
+//			return (pixel(renderer, x, y));
+//		}
+//		else {
+//			return (vline(renderer, x, y - ry, y + ry));
+//		}
+//	}
+//	else {
+//		if (ry == 0) {
+//			return (hline(renderer, x - rx, x + rx, y));
+//		}
+//	}
+//
+//	/*
+//	 * Adjust overscan
+//	 */
+//	rxi = rx;
+//	ryi = ry;
+//	if (rxi >= 512 || ryi >= 512)
+//	{
+//		ellipseOverscan = DEFAULT_ELLIPSE_OVERSCAN / 4;
+//	}
+//	else if (rxi >= 256 || ryi >= 256)
+//	{
+//		ellipseOverscan = DEFAULT_ELLIPSE_OVERSCAN / 2;
+//	}
+//	else
+//	{
+//		ellipseOverscan = DEFAULT_ELLIPSE_OVERSCAN / 1;
+//	}
+//
+//	/*
+//	 * Top/bottom center points.
+//	 */
+//	oldX = scrX = 0;
+//	oldY = scrY = ryi;
+//	result |= _drawQuadrants(renderer, x, y, 0, ry, f);
+//
+//	/* Midpoint ellipse algorithm with overdraw */
+//	rxi *= ellipseOverscan;
+//	ryi *= ellipseOverscan;
+//	rx2 = rxi * rxi;
+//	rx22 = rx2 + rx2;
+//	ry2 = ryi * ryi;
+//	ry22 = ry2 + ry2;
+//	curX = 0;
+//	curY = ryi;
+//	deltaX = 0;
+//	deltaY = rx22 * curY;
+//
+//	/* Points in segment 1 */
+//	error = ry2 - rx2 * ryi + rx2 / 4;
+//	while (deltaX <= deltaY)
+//	{
+//		curX++;
+//		deltaX += ry22;
+//
+//		error += deltaX + ry2;
+//		if (error >= 0)
+//		{
+//			curY--;
+//			deltaY -= rx22;
+//			error -= deltaY;
+//		}
+//
+//		scrX = curX / ellipseOverscan;
+//		scrY = curY / ellipseOverscan;
+//		if ((scrX != oldX && scrY == oldY) || (scrX != oldX && scrY != oldY)) {
+//			result |= _drawQuadrants(renderer, x, y, scrX, scrY, f);
+//			oldX = scrX;
+//			oldY = scrY;
+//		}
+//	}
+//
+//	/* Points in segment 2 */
+//	if (curY > 0)
+//	{
+//		curXp1 = curX + 1;
+//		curYm1 = curY - 1;
+//		error = ry2 * curX * curXp1 + ((ry2 + 3) / 4) + rx2 * curYm1 * curYm1 - rx2 * ry2;
+//		while (curY > 0)
+//		{
+//			curY--;
+//			deltaY -= rx22;
+//
+//			error += rx2;
+//			error -= deltaY;
+//
+//			if (error <= 0)
+//			{
+//				curX++;
+//				deltaX += ry22;
+//				error += deltaX;
+//			}
+//
+//			scrX = curX / ellipseOverscan;
+//			scrY = curY / ellipseOverscan;
+//			if ((scrX != oldX && scrY == oldY) || (scrX != oldX && scrY != oldY)) {
+//				oldY--;
+//				for (; oldY >= scrY; oldY--) {
+//					result |= _drawQuadrants(renderer, x, y, scrX, oldY, f);
+//					/* prevent overdraw */
+//					if (f) {
+//						oldY = scrY - 1;
+//					}
+//				}
+//				oldX = scrX;
+//				oldY = scrY;
+//			}
+//		}
+//
+//		/* Remaining points in vertical */
+//		if (!f) {
+//			oldY--;
+//			for (; oldY >= 0; oldY--) {
+//				result |= _drawQuadrants(renderer, x, y, scrX, oldY, f);
+//			}
+//		}
+//	}
+//
+//	return (result);
+//}
+//int filledEllipseRGBA(SDL_Renderer* renderer, Sint16 x, Sint16 y, Sint16 rx, Sint16 ry, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+//{
+//	return _ellipseRGBA(renderer, x, y, rx, ry, r, g, b, a, 1);
+//}
+//int filledCircleColor(SDL_Renderer* renderer, Sint16 x, Sint16 y, Sint16 rad, Uint32 color)
+//{
+//	Uint8* c = (Uint8*)&color;
+//	return filledEllipseRGBA(renderer, x, y, rad, rad, c[0], c[1], c[2], c[3]);
+//}
 
 SDL_Window* window = SDL_CreateWindow("Button Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-// Create a renderer for the window
 SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 TTF_Font* font;
 using namespace std;
@@ -390,7 +389,8 @@ int main(int argc, char* argv[]) {
 		SDL_RenderCopy(renderer, buttonTextTexture, NULL, &buttonRect);
 		Normal_Letters.Display_Button();
 		SDL_SetRenderDrawColor(renderer, 47, 15, 154, 255);
-		filledCircleColor(renderer, 10 + 50, 10 + 50, 50, 0xFF0000FF);
+		//filledCircleColor(renderer, 10 + 50, 10 + 50, 50, 0xFF0000FF);
+		filledCircleRGBA(renderer, 10 + 50, 10 + 50,50, 100,255, 98, Transparency);
 		//filledCircleColor(renderer, (Normal_Letters.get_Position().X+50), Normal_Letters.get_Position().Y + 50, 15, 0xFF0000FF);
 		SDL_RenderCopy(renderer, buttonTextTexture, NULL, &buttonRect);
 		SDL_RenderPresent(renderer);
